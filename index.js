58ff58dccc4ae0dca4bdf115f50d0f65f9a10f27
minator10000 <ivyivvy1@outlook.com>
2025-04-07 08:45:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
-require("dotenv\config");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
